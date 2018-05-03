9cbadf6769828d78ec71b67f567957a5dc2051cd
Larry Botha <larry@fixate.it>
2018-05-03 08:26:57 +0200

    add authentication

--- api/src/app.js
+++ api/src/app.js
@@ -15,32 +15,40 @@ const services = require('./services');
 const appHooks = require('./app.hooks');
 const channels = require('./channels');
 
+const authentication = require('./authentication');
+
+const mongoose = require('./mongoose');
+
 const app = express(feathers());
 
 // Load app configuration
-app.configure(configuration());
-// Enable CORS, security, compression, body parsing
-app.use(cors());
-app.use(helmet());
-app.use(compress());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app
+  .configure(configuration())
+  // Enable CORS, security, compression, body parsing
+  .use(cors())
+  .use(helmet())
+  .use(compress())
+  .use(express.json())
+  .use(express.urlencoded({extended: true}))
+  .configure(authentication)
 
-// Set up Plugins and providers
-app.configure(express.rest());
-app.configure(socketio());
+  .configure(mongoose)
 
-// Configure other middleware (see `middleware/index.js`)
-app.configure(middleware);
-// Set up our services (see `services/index.js`)
-app.configure(services);
-// Set up event channels (see channels.js)
-app.configure(channels);
+  // Set up Plugins and providers
+  .configure(express.rest())
+  .configure(socketio())
 
-// Configure a middleware for 404s and the error handler
-app.use(express.notFound());
-app.use(express.errorHandler({logger}));
+  // Configure other middleware (see `middleware/index.js`)
+  .configure(middleware)
+  // Set up our services (see `services/index.js`)
+  .configure(services)
+  // Set up event channels (see channels.js)
+  .configure(channels)
 
-app.hooks(appHooks);
+  // Configure a middleware for 404s and the error handler
+  .use(express.notFound())
+  .use(express.errorHandler({logger}))
+
+  .hooks(appHooks);
 
 module.exports = app;
