362f46c1842186e14903e589dcaa0c2dc28a3ede
Larry Botha <larry@fixate.it>
2018-05-21 22:35:44 +0200

    use xstream store and create effects

--- client/src/assets/js/store.js
+++ client/src/assets/js/store.js
@@ -1,12 +1,12 @@
-// import createStore from './lib/xstream-store';
-import {createStore} from './lib/store2';
-
-import createCounter$ from './streams/counter';
+import createStore from './lib/xstream-store';
+import {createCounterState$, createCounterEffect$} from './streams/counter';
 
 const streamCreators = {
-  counter: createCounter$,
+  counter: createCounterState$,
 };
 
-const store = createStore(streamCreators);
+const effectCreators = [createCounterEffect$];
+
+const store = createStore(streamCreators, effectCreators);
 
 export default store;
