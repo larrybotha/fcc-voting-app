5d9726e27e2832f44e64c2b85a72a4aabead7ba8
Larry Botha <larry@fixate.it>
2018-06-25 09:24:47 +0200

    move xstream provider to app component

--- client/src/assets/js/components/App.js
+++ client/src/assets/js/components/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import XstreamContext from 'react-xstream-store';
+import {Provider} from 'react-xstream-store';
 
 import Home from './routes/Home';
 import Login from './routes/Login';
 import Signup from './routes/Signup';
 import Votes from './routes/votes';
 
-import * as counterActions from '../actions/counter';
-
 import Menu from './Menu';
 
 import routes from '../routes';
 import {auth$} from '../streams/auth';
+import store from '../store';
 
 const actions = {
   increment: {type: counterActions.INCREMENT, value: 1},
@@ -37,33 +36,20 @@ class App extends React.Component {
     const {count, dispatch, decrement, increment, request} = this.props;
 
     return (
-      <BrowserRouter>
-        <>
-          <XstreamContext.Consumer actions={actions}>
-            {props => {
-              const {counter, dispatch, decrement, increment, request} = props;
+      <Provider store={store}>
+        <BrowserRouter>
+          <>
+            <Menu />
 
-              return (
-                <div>
-                  <button onClick={() => dispatch(decrement)}>-</button>
-                  {counter.count}
-                  <button onClick={() => dispatch(increment)}>+</button>
-                  <button onClick={() => dispatch(request)}>request</button>
-                </div>
-              );
-            }}
-          </XstreamContext.Consumer>
-
-          <Menu />
-
-          <Switch>
-            <Route exact path={routes.home} component={Home} />
-            <Route exact path={routes.login} component={Login} />
-            <Route exact path={routes.signup} component={Signup} />
-            <Route path={routes.votes} component={Votes} />
-          </Switch>
-        </>
-      </BrowserRouter>
+            <Switch>
+              <Route exact path={routes.home} component={Home} />
+              <Route exact path={routes.login} component={Login} />
+              <Route exact path={routes.signup} component={Signup} />
+              <Route path={routes.votes} component={Votes} />
+            </Switch>
+          </>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- client/src/assets/js/index.js
+++ client/src/assets/js/index.js
@@ -2,16 +2,8 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import App from './components/App';
-import XstreamContext from 'react-xstream-store';
 
-import store from './store';
-
-render(
-  <XstreamContext.Provider value={store}>
-    <App />
-  </XstreamContext.Provider>,
-  document.querySelector('.js-mount')
-);
+render(<App />, document.querySelector('.js-mount'));
 
 if (module.hot) {
   module.hot.accept();
